632cf8503894fcbcc31147a9399e0e46f63fd4f0
Nox <waleed.ahmadmunir@gmail.com>
2025-04-15 13:04:18 +0500

    DB Connected

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const db = require("./db");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("✅ Server running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server on http://localhost:${PORT}`));
